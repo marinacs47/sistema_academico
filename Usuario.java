9c9ef9ff2d388d93c500cf11b9fa122e6c3b1ba0
Marina Cruvinel Saraiva <marina.cs.0407@gmail.com>
2025-11-12 12:33:41 -0300

    Atualizar o Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -10,24 +10,28 @@ class Usuario implements IAutenticacao(){
 	private TipoUsuario tipoUsuario;
 	private static List<Usuario> usuarios = new ArrayList<>();
 
-	static{
-						usuarios = new ArrayList<Usuario>();
-						usuarios.add(new Usuario("Administrador"));
-						usuarios.add(new Usuario("Professor1"));
-						usuarios.add(new Usuario("Professor2"));
-						usuarios.add(new Usuario("Aluno1"));
-						usuarios.add(new Usuario("Aluno2"));
-						usuarios.add(new Usuario("Aluno3"));
-						usuarios.add(new Usuario("Aluno4"));
-						usuarios.add(new Usuario("Aluno5"));
-						usuarios.add(new Usuario("Turma1"));
-	}
-
-
 	public Usuario(String nome, String login, String senha, TipoUsuario tipoUsuario){
 		super(login, senha);
 		this.nome = nome;
 		this.tipoUsuario = tipoUsuario;
+	}
+
+	static{
+						TipoUsuario Administrador = new TipoUsuario("Administrador");
+						TipoUsuario Professor = new TipoUsuario("Professor");
+						TipoUsuario Aluno = new TipoUsuario("Aluno");
+						TipoUsuario Turma = new TipoUsuario("Turma");
+
+						usuarios.add(new Usuario("ADMIN", Administrador));
+						usuarios.add(new Usuario("Professor1", Professor));
+						usuarios.add(new Usuario("Professor2", Professor));
+						usuarios.add(new Usuario("Aluno1", Aluno));
+						usuarios.add(new Usuario("Aluno2", Aluno));
+						usuarios.add(new Usuario("Aluno3", Aluno));
+						usuarios.add(new Usuario("Aluno4", Aluno));
+						usuarios.add(new Usuario("Aluno5", Aluno));
+						usuarios.add(new Usuario("Turma1", Turma));
+												
 	}
 
 	public getTipoUsuario getTipoUsuario(){
