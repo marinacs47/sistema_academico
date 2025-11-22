29c0ec2e5abefc30572017f7ea2177a379a33970
Miguel Arcanjo Nascimento Meriguete <miguelmeriguete@gmail.com>
2025-11-21 23:15:01 -0300

    Professor.java

--- Professor.java
+++ Professor.java
@@ -1,35 +1,42 @@
-package sistema_academico.modelo;
+package modelo.sistema_academico;
 
 import java.util.ArrayList;
 import java.util.List;
+        
+public class Professor extends Pessoa {
+    private int siape;
+    private static List<Professor> professores = new ArrayList<>();
 
-public class Professor extends Pessoa{
-	private int siape;
-	private List<Professor> professores = new ArrayList<>(); 
+    public Professor(int siape,String nome){
+        super(nome);
+        this.siape = siape;
+    }
+    
+    public int getSiape(){
+        return siape;
+    }
+    
+    public static List<Professor> getProfessores(){
+        return professores;
+    }
+    
+    public static void inserir(Professor professor){
+        if(professor != null){
+         professores.add(professor);   
+        }
+    }
 
-	public Professor(int siape, String nome){
-		super(nome);
-		this.siape = siape;
-	}
-
-	public int getSiape(){
-		return siape;
-	}
-
-	public List<Professor> getProfessores(){
-		return professores;
-	}
-
-	public void inserir(Professor professor){
-		professores.add(professor);
-	}
-
-	public void remover(Professor professor){
-		professores.remove(professor);
-	}
-
-	public Professor obter(Professor professor){
-		professores.get(professor);
-	}
-
+    public static boolean remover(Professor professor){
+        return professores.remove(professor);     
+    }
+    
+    public static Professor obter(Professor professor) {
+    for (Professor p : professores) {
+        if (p.equals(professor)) {
+            return p;
+        }
+    }
+        return null;
+    }
+    
 }
