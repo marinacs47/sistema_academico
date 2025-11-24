cfc341fb297d70a55717adf0551faa7b6e170fe7
Marina Cruvinel Saraiva <marina.cs.0407@gmail.com>
2025-11-24 13:56:33 -0300

    Criar Principal.java

---
+++ Principal.java
@@ -0,0 +1,9 @@
+package sistema_academico;
+
+import sistema_academico.controle.AcademicoControlador;
+
+class Principal{
+		public static void main(String[] args){
+			new AcademicoControlador().inicializar();
+		}
+} 
