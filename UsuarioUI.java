6725e4d7afa0d2cb994ffb7919c1686262c71667
Marina Cruvinel Saraiva <marina.cs.0407@gmail.com>
2025-11-24 14:37:29 -0300

    Atualizar o UsuarioUI.java

--- UsuarioUI.java
+++ UsuarioUI.java
@@ -1,1 +1,30 @@
 package sistema_academico.visao;
+
+import sistema_academico.modelo.Usuario;
+import sistema_academico.modelo.TipoUsuario;
+import sistema_academico.modelo.Funcionalidade;
+
+import javax.swing.JOptionPane;
+
+public class UsuarioUI{
+	
+	public static Usuario autenticar() {
+		String login = JOptioonPane.showInputDialog(null, "Login:");
+		if(login == null){
+			return null;
+		}
+	
+		String senha = JOptioonPane.showInputDialog(null, "Senha:");
+		if(senha == null){
+			return null;
+		}
+
+		Usuario u = Usuario.obter(login, senha);
+		if(u == null){
+			JOptionPane.showMessageDialog(null, "Login ou senha inválidos");
+			return null;
+		}
+			return u;
+	}
+
+}
