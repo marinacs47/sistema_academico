41dd1c4f3abb4f761524381760e0aab2deb3b477
Marina Cruvinel Saraiva <marina.cs.0407@gmail.com>
2025-11-24 14:02:55 -0300

    Atualizar o AcademicoControlador.java

--- AcademicoControlador.java
+++ AcademicoControlador.java
@@ -1,1 +1,14 @@
-package
+package sistema_academico.controle;
+
+import sistema_academico.modelo.Usuario;
+import sistema_academico.modelo.Aluno;
+import sistema_academico.modelo.Professor;
+import sistema_academico.modelo.Turma;
+import sistema_academico.visao.UsuarioUI;
+import sistema_academico.visao.AlunoUI;
+import sistema_academico.visao.AlunoUI;
+import sistema_academico.visao.ProfessorUI;
+import sistema_academico.visao.TurmaUI;
+
+import java.util.List;
+import java.util.ArrayList;
