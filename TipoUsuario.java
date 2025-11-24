8cd5e2b514230e7484bea82b15d3c90712fabb68
Marina Cruvinel Saraiva <marina.cs.0407@gmail.com>
2025-11-24 13:17:33 -0300

    Atualizar o TipoUsuario.java

--- TipoUsuario.java
+++ TipoUsuario.java
@@ -21,7 +21,16 @@ public class TipoUsuario {
 	}
 
 	public void definirFuncionalidades(){
-		
+		if(nome.equalsIgnoreCase("Administrador")){
+			funcionalidades.add(Funcionalidade.getFuncionalidade("CA"));
+			funcionalidades.add(Funcionalidade.getFuncionalidade("CP"));
+			funcionalidades.add(Funcionalidade.getFuncionalidade("CT"));
+			funcionalidades.add(Funcionalidade.getFuncionalidade("LA"));
+			funcionalidades.add(Funcionalidade.getFuncionalidade("LP"));
+			funcionalidades.add(Funcionalidade.getFuncionalidade("LT"));
+			funcionalidades.add(Funcionalidade.getFuncionalidade("RA"));
+			funcionalidades.add(Funcionalidade.getFuncionalidade("RP"));
+			funcionalidades.add(Funcionalidade.getFuncionalidade("RT"));
+		}
 	}
-
 }
