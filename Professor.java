8507aae07541ea9f70aa4b0c129c10771afedef4
Miguel Arcanjo Nascimento Meriguete <miguelmeriguete@gmail.com>
2025-11-28 19:55:02 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -5,14 +5,16 @@ import java.util.List;
         
 public class Professor extends Pessoa {
     private int siape;
-    private static List<Professor> professores = new ArrayList<>();
+    private static List<Professor> professores;
 
     public Professor(int siape,String nome){
         super(nome);
         this.siape = siape;
-        professores.add(this);   
     }
     
+     static{
+       professores = new ArrayList<>();
+    }
     public int getSiape(){
         return siape;
     }
@@ -22,7 +24,7 @@ public class Professor extends Pessoa {
     }
     
     public static void inserir(Professor professor){
-        if(professor != null){ //verificando se a lista tá vazia
+        if(professor != null){
          professores.add(professor);   
         }
     }
@@ -32,15 +34,12 @@ public class Professor extends Pessoa {
     }
     
     public static Professor obter(Professor professor) {
-    for (Professor p : professores) { //to buscando um professor da lista de professores
-        if (p.equals(professor)) { //aqui to comparando um professor p com o passado do método
-            return p; //se for igual, retorna p
+    for (Professor p : professores) {
+        if (p.equals(professor)) {
+            return p;
         }
     }
-        return null; // se nao,retorna nulo
+        return null;
     }
     
 }
-
-
-
