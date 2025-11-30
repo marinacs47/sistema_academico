b0d67bb46cc0c9e8581f396e1fb1a17b725fdb29
Marina Cruvinel Saraiva <marina.cs.0407@gmail.com>
2025-11-29 23:49:54 -0300

    Atualizar o TipoUsuario.java

--- TipoUsuario.java
+++ TipoUsuario.java
@@ -1,36 +1,37 @@
 package sistema_academico.modelo;
 
+import sistema_academico.modelo.TipoUsuario;
 import java.util.List;
 import java.util.ArrayList;
 
 public class TipoUsuario {
-	private String nome;
-	private List<Funcionalidade> funcionalidades = new ArrayList<>();
+private String nome;
+private List<Funcionalidade> funcionalidades = new ArrayList<>();
 
-	public TipoUsuario(String nome) {
-		this.nome = nome;
-		definirFuncionalidades();
-	}
+public TipoUsuario(String nome) {
+this.nome = nome;
+definirFuncionalidades();
+}
 
-	public String getNome(){
-		return nome;
-	}
+public String getNome(){
+return nome;
+}
 
-	public List<Funcionalidade> getFuncionalidades(){
-		return funcionalidades;
-	}
+public List<Funcionalidade> getFuncionalidades(){
+return funcionalidades;
+}
 
-	public void definirFuncionalidades(){
-		if(nome.equalsIgnoreCase("Administrador")){
-			funcionalidades.add(Funcionalidade.getFuncionalidade("CA"));
-			funcionalidades.add(Funcionalidade.getFuncionalidade("CP"));
-			funcionalidades.add(Funcionalidade.getFuncionalidade("CT"));
-			funcionalidades.add(Funcionalidade.getFuncionalidade("LA"));
-			funcionalidades.add(Funcionalidade.getFuncionalidade("LP"));
-			funcionalidades.add(Funcionalidade.getFuncionalidade("LT"));
-			funcionalidades.add(Funcionalidade.getFuncionalidade("RA"));
-			funcionalidades.add(Funcionalidade.getFuncionalidade("RP"));
-			funcionalidades.add(Funcionalidade.getFuncionalidade("RT"));
-		}
-	}
+public void definirFuncionalidades(){
+if(nome.equalsIgnoreCase("Administrador")){
+funcionalidades.add(Funcionalidade.getFuncionalidade("CA"));
+funcionalidades.add(Funcionalidade.getFuncionalidade("CP"));
+funcionalidades.add(Funcionalidade.getFuncionalidade("CT"));
+funcionalidades.add(Funcionalidade.getFuncionalidade("LA"));
+funcionalidades.add(Funcionalidade.getFuncionalidade("LP"));
+funcionalidades.add(Funcionalidade.getFuncionalidade("LT"));
+funcionalidades.add(Funcionalidade.getFuncionalidade("RA"));
+funcionalidades.add(Funcionalidade.getFuncionalidade("RP"));
+funcionalidades.add(Funcionalidade.getFuncionalidade("RT"));
+}
+}
 }
