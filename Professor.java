90b4feaf93ea451a1517e16f43289b2edeb89e4e
Marina Cruvinel Saraiva <marina.cs.0407@gmail.com>
2025-11-29 23:49:23 -0300

    Atualizar o Professor.java

--- Professor.java
+++ Professor.java
@@ -2,44 +2,49 @@ package sistema_academico.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
-        
+       
 public class Professor extends Pessoa {
     private int siape;
     private static List<Professor> professores;
+    
+    static{
+        professores = new ArrayList<>();
+    }
 
     public Professor(int siape,String nome){
         super(nome);
         this.siape = siape;
+        professores.add(this);  
     }
-    
-     static{
-       professores = new ArrayList<>();
-    }
+   
     public int getSiape(){
         return siape;
     }
-    
+   
     public static List<Professor> getProfessores(){
         return professores;
     }
-    
+   
     public static void inserir(Professor professor){
-        if(professor != null){
-         professores.add(professor);   
+        if(professor != null){ //verificando se a lista tá vazia
+         professores.add(professor);  
         }
     }
 
     public static boolean remover(Professor professor){
-        return professores.remove(professor);     
+        return professores.remove(professor);    
     }
-    
+   
     public static Professor obter(Professor professor) {
-    for (Professor p : professores) {
-        if (p.equals(professor)) {
-            return p;
+    for (Professor p : professores) { //to buscando um professor da lista de professores
+        if (p.equals(professor)) { //aqui to comparando um professor p com o passado do método
+            return p; //se for igual, retorna p
         }
     }
-        return null;
+        return null; // se nao,retorna nulo
     }
-    
+    @Override
+    public String toString() {
+    return "SIAPE: " + siape + " | Nome: " + getNome();
+}   
 }
